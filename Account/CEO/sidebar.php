7c8bd89b9d4ac5954a33596b59197776a42fe6c3
Your Name <you@example.com>
2022-08-05 23:21:34 +0530

    ffghj

---
+++ Account/CEO/sidebar.php
@@ -0,0 +1,46 @@
+<!-- Sidebar -->
+<nav
+       id="sidebarMenu"
+       class="collapse d-lg-block sidebar collapse bg-white"
+       >
+    <div class="position-sticky" id="nav_mp_side">
+      <div class="list-group list-group-flush mx-3 mt-4">
+        <a
+           href="dashboard.php"
+           class="list-group-item list-group-item-action py-2 ripple"
+           aria-current="true"
+           >
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i
+            ><span>Dashboard</span>
+        </a>
+        <a
+           href="Application.php"
+           class="list-group-item list-group-item-action py-2 ripple"
+           ><i class="fas fa-envelope-open fa-fw me-3"></i><span>Application</span></a
+          >
+        <a
+           href="#"
+           class="list-group-item list-group-item-action py-2 ripple"
+           ><i class="fas fa-newspaper fa-fw me-3"></i
+          ><span>History</span></a
+          >
+        <a
+           href="#"
+           class="list-group-item list-group-item-action py-2 ripple"
+           >
+          <i class="fas fa-key fa-fw me-3"></i><span>Password</span>
+        </a>
+		<a
+        href="http://192.168.60.4:90/"
+        class="list-group-item list-group-item-action py-2 ripple"
+        ><i class="fas fa-search fa-fw me-3"></i><span>Subharti ERP</span></a
+        >
+        <a
+           href="../../Logout.php"
+           class="list-group-item list-group-item-action py-2 ripple"
+           ><i class="fas fa-sign-out fa-fw me-3"></i><span>Log Out</span></a
+          >
+      </div>
+    </div>
+  </nav>
+  <!-- Sidebar -->
