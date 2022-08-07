b783e5ba98ada314183f95b0dc07b2d829b31a08
MD Amzad <md.amzad.site.btech.cse.2@subhartiuniversit.onmicrosoft.com>
2022-08-08 04:34:36 +0530

    done

--- Account/CEO/Letter.php
+++ Account/CEO/Letter.php
@@ -1,4 +1,29 @@
 <?php
+
+function GetFirstLetter(string $string) {
+    $words = explode(" ", strtoupper($string));
+    $acronym = "";
+
+    foreach ($words as $w) {
+    $acronym .= mb_substr($w, 0, 1);
+    }
+    return $acronym;
+}
+
+$connect = mysqli_connect("localhost", "root", "");
+$connect->select_db("svsuapp");
+session_start();
+$query = "select email from users where username='".$_SESSION["username"]."'";
+$result = $connect->query($query);
+$data = $result->fetch_assoc();
+
+$position = $_POST['user_type'];
+$college = $_POST['college'];
+$subject = $_POST['subject'];
+$msg = $_POST['msg'];
+$email = $data['email'];
+
+
 ob_start();
 require_once "../../fpdf/fpdf.php";
 $pdf = new FPDF();
@@ -8,13 +33,13 @@ $pdf->Image("../../Resource/subharti logo.jpg", 10, 5, 30, 30);
 
 $pdf->SetFont('Arial', 'B', 15);
 //         width, height, text, border, new line, align
-$pdf->Cell(0, 5, "TRAINING AND PLACEMENT DEPARTMENT", 0, 1, "C");
+$pdf->Cell(0, 5, strtoupper($college), 0, 1, "C");
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(0, 5, "SWAMI VIVEKANAND SUBHARTI UNIVERSITY", 0, 1, "C");
 $pdf->SetFont('Arial', 'B', 8);
 $pdf->Cell(0, 3, "(Established under U.P. Govt. Act no. 29 of 2008 and approved under section 2(f) of UGC Act 1956)", 0, 1, "C");
 $pdf->Cell(0, 3, "Ph. 0121-2439043, 2439052, +91 7302319995; Telfax: 0121-2439067", 0, 1, "C");
-$pdf->Cell(0, 3, "Email: placement@subharti.org, Website: www.subharti.org", 0, 1, "C");
+$pdf->Cell(0, 3, "Email: ".$email.", Website: www.subharti.org", 0, 1, "C");
 
 $pdf->Image("../../Resource/naac.png", 165, 5, 40, 28);
 $pdf->SetLineWidth(1);
@@ -22,9 +47,29 @@ $pdf->Line(0, 35, $pdf->GetPageWidth(), 35);
 
 $pdf->Ln(8);
 $pdf->SetFont('Arial', 'B', 12);
-$pdf->Cell(0, 10, "Ref. No. SVSU/SPC/".date("Y"), 0, 0, "L");
+$pdf->Cell(0, 10, "Ref. No. SVSU/".GetFirstLetter($college)."/".date("Y"), 0, 0, "L");
 date_default_timezone_set('Asia/Calcutta');
 $pdf->Cell(0, 10, "Date: ".date("d-m-Y h:i:sa"), 0, 1, "R");
+
+$pdf->SetY(60);
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(0, 10, "To,", 0, 1, "L");
+$pdf->SetX(20);
+$pdf->SetFont('Arial', '', 12);
+$pdf->Cell(0, 7, "The ".$position, 0, 1, "L");
+$pdf->SetX(20);
+$pdf->Cell(0, 7, $college, 0, 1, "L");
+$pdf->SetX(20);
+$pdf->Cell(0, 7, "SVSU, Meerut", 0, 1, "L");
+
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(0, 7, "Subject:- ".$subject, 0, 1, "L");
+
+$pdf->SetX(20);
+$pdf->SetFont('Arial', '', 12);
+// $pdf->Cell(0, 7, $msg, 0, 1, "L");
+$pdf->MultiCell($pdf->GetPageWidth()-40, 10, $msg, 0, "J", false);
+
 
 $pdf->Line(0, $pdf->GetPageHeight()-35, $pdf->GetPageWidth(), $pdf->GetPageHeight()-35);
 $pdf->SetY($pdf->GetPageHeight()-31);
--- Account/CEO/Post_application.php
+++ Account/CEO/Post_application.php
@@ -24,35 +24,45 @@
 <body>
 <main style="margin-top: 58px" id="nav_mp">
   <div class="container pt-4">
-  <select name="user_type" id="user_type" style="text-align: center">
-        <option value="Select User">Post To </option>
-         <option value="VC">VC</option>
-        <option value="Registrar">Registrar</option>
-        <option value="Principle">Principle</option>
-  </select>
-  <select name="user_type" id="user_type">
-    <option value="Select User" style="text-align: center">Select Collage</option>
-    <option value="CEO">Faculty of Medicine </option>
-    <option value="VC"> Faculty of Dental Sciences</option>
-    <option value="Registrar"> Faculty of Nursing</option>
-    <option value="Principle">Faculty of Physiotherapy & Allied Sciences</option>
-    <option value="Principle"> Faculty of AYUSH</option>
-    <option value="Principle"> Faculty of Pharmacy</option>
-    <option value="Principle"> Faculty of Law</option>
-    <option value="Principle">Faculty of Engineering</option>
-    <option value="Principle"> Faculty of Science Faculty of Education</option>
-    <option value="Principle">Subharti College of Management & Commerce</option>
-    <option value="Principle"> Faculty of Fine Arts </option>
-    <option value="Principle"> Faculty of Arts & Social Science</option>
-    <option value="Principle">School of Buddhist Studies</option>
-    <option value="Principle"> Subharti College of Hotel Management</option>
-    <option value="Principle"> Subharti Polytechnic College</option>
-  </select>
-    <div class="md-form mb-4 pink-textarea active-pink-textarea">
-        <i class="fas fa-angle-double-right prefix"></i>
-        <textarea id="form21" class="md-textarea form-control" rows="18"placeholder="Write your application here..."></textarea>
-    </div>
-<button class="btn purple-gradient btn-lg" id="New_Registration" name="submit">POST APPLICATION</button>
+    <form action="Letter.php" method="post">
+    <select name="user_type" id="user_type" style="text-align: center">
+          <option value="Select User">Post To </option>
+          <option value="VC">VC</option>
+          <option value="Registrar">Registrar</option>
+          <option value="Principle">Principle</option>
+    </select>
+    <select name="college" id="college">
+      <option value="" style="text-align: center">Select Collage</option>
+      <option value="Faculty of Medicine">Faculty of Medicine </option>
+      <option value="Faculty of Dental Sciences"> Faculty of Dental Sciences</option>
+      <option value="Faculty of Nursing"> Faculty of Nursing</option>
+      <option value="Faculty of Physiotherapy & Allied Sciences">Faculty of Physiotherapy & Allied Sciences</option>
+      <option value="Faculty of AYUSH"> Faculty of AYUSH</option>
+      <option value="Faculty of Pharmacy"> Faculty of Pharmacy</option>
+      <option value="Faculty of Law"> Faculty of Law</option>
+      <option value="Faculty of Engineering">Faculty of Engineering and Technology</option>
+      <option value="Faculty of Science Faculty of Education"> Faculty of Science Faculty of Education</option>
+      <option value="Subharti College of Management & Commerce">Subharti College of Management & Commerce</option>
+      <option value="Faculty of Fine Arts"> Faculty of Fine Arts </option>
+      <option value="Faculty of Arts & Social Science"> Faculty of Arts & Social Science</option>
+      <option value="School of Buddhist Studies">School of Buddhist Studies</option>
+      <option value="Subharti College of Hotel Management"> Subharti College of Hotel Management</option>
+      <option value="Subharti Polytechnic College"> Subharti Polytechnic College</option>
+    </select>
+      <div class="md-form mb-4 pink-textarea active-pink-textarea">
+          <i class="fas fa-angle-double-right prefix"></i>
+
+          <label for="to">To</label>
+          <input type="text" id="to" name="to"class="md-input-text form-control" value="The ">
+
+          <label for="subject">Subject</label>
+          <input type="text" name="subject" id="subject" class="md-input-text form-control">
+
+          <label for="msg">Respected Sir/Madam,</label>
+          <textarea id="msg"name = "msg" class="md-textarea form-control" rows="18"placeholder="Write your application here..."></textarea>
+      </div>
+      <button class="btn purple-gradient btn-lg" id="New_Registration" name="submit">POST APPLICATION</button>
+    </form>
   </div>
 </main>
 </body>
